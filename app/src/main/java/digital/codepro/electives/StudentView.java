99c760fe601b62153e9e9cf6e0a0c11ed99a42b8
plex303 <rmutani@gmail.com>
2020-11-10 19:36:48 +0200

    first commit

---
+++ app/src/main/java/digital/codepro/electives/StudentView.java
@@ -0,0 +1,13 @@
+package digital.codepro.electives;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_view);
+    }
+}
